5b34039b95eb1e31552d4c79d26dc1be5e32c41b
Prashant Sahatiya <prashantsahatiya18@gmail.com>
2023-05-17 11:26:29 +0530

    Uploaded TabLayout

---
+++ PPITabLayout/app/src/main/java/com/example/ppitablayout/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.ppitablayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    ViewPager2 viewPager2;
+    TabLayout tabLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager2 = findViewById(R.id.myviewpager);
+        tabLayout = findViewById(R.id.mytablayout);
+
+        MyViewPagerAdapter myViewPagerAdapter = new MyViewPagerAdapter(MainActivity.this);
+        viewPager2.setAdapter(myViewPagerAdapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                tabLayout.getTabAt(position).select();
+            }
+        });
+
+
+    }
+}
